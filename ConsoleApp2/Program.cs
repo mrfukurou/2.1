2a449a3358dcf47f0bb249fa597db562f76d3b3a
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-07 20:46:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("x=");
+                float x = float.Parse(Console.ReadLine());
+                Console.Write("y=");
+                float y = float.Parse(Console.ReadLine());
+                if (x < 0 && x > -15 && y < 0 && y > -15)
+                    Console.WriteLine("вне");
+                else if (((x == -15 || x == 0) && y <= 0 && y >= (-15)) || ((y == -15 || y == 0) && x <= 0 && x >= (-15)))
+                    Console.WriteLine("на границе");
+                else Console.WriteLine("внутри");
+            }
+            catch { Console.WriteLine("Некорректный ввод данных"); }
+
+        }
+    }
+}
